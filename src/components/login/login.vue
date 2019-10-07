ca4177117b193ebe888571bc3bc6ec5820d96e77
zhangzhimiao <1584221568@qq.com>
2019-10-07 22:27:31 +0800

    add nodemon

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -97,6 +97,8 @@ export default {
           width: 100%;
           height: 40px;
           margin-top: 15px;
+          border-radius: 10px;
+          outline: none;
         }
       }
       &-register {
