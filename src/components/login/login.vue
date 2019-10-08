cde2f052527496f0b54191d4b5111d28cdd27fee
bj.develop.intern7 <bj.develop.intern7@yff.com>
2019-10-08 14:44:19 +0800

    add editor demo

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -7,13 +7,37 @@
           <input type="text" placeholder="请输入用户名：)" v-model="name" />
           <br />
           <input type="password" placeholder="请输入密码" v-model="password" />
+          <br />
+          <input
+            v-show="!isLogin"
+            type="password"
+            placeholder="请确认密码"
+            v-model="enablepassword"
+          />
         </div>
       </div>
       <div class="login-wrap-operate">
         <div class="login-wrap-operate-submit">
-          <input type="button" value="登录" @click="login" />
+          <input type="button" value="登录" @click="login" v-show="!isLogin" />
+          <input
+            type="button"
+            value="登录"
+            @click="enablelogin"
+            v-show="isLogin"
+          />
         </div>
-        <div class="login-wrap-operate-register" @click="register">
+        <div
+          class="login-wrap-operate-register"
+          @click="register"
+          v-show="isLogin"
+        >
+          <input type="button" value="注册" />
+        </div>
+        <div
+          class="login-wrap-operate-register"
+          @click="enableregister"
+          v-show="!isLogin"
+        >
           <input type="button" value="注册" />
         </div>
       </div>
@@ -27,17 +51,37 @@ export default {
   data() {
     return {
       name: "",
-      password: ""
+      password: "",
+      enablepassword: "",
+      isLogin: true
     };
   },
   methods: {
     login() {
+      this.isLogin = true;
+    },
+    enablelogin() {
       service.login(this.name, this.password).then(d => {
         // eslint-disable-next-line no-console
-        console.log(d.data);
+        console.log(d);
       });
     },
     register() {
+      this.isLogin = false;
+    },
+    enableregister() {
+      if (this.name.length < 2) {
+        alert("用户名必须超过两位");
+        return;
+      }
+      if (this.password !== this.enablepassword) {
+        alert("两次密码输入不一致");
+        return;
+      }
+      if (this.password.length < 6) {
+        alert("密码长度必须超过六位");
+        return;
+      }
       service.register(this.name, this.password).then(d => {
         // eslint-disable-next-line no-console
         console.log(d);
@@ -57,7 +101,7 @@ export default {
   align-items: center;
   &-wrap {
     width: 380px;
-    height: 400px;
+    height: 450px;
     background: rgba(255, 255, 255, 0.6);
     border-radius: 6px;
     &-title {
@@ -73,7 +117,7 @@ export default {
       display: flex;
       justify-content: center;
       align-items: center;
-      height: 150px;
+      height: 200px;
       div {
         width: 80%;
       }
@@ -104,6 +148,7 @@ export default {
           margin-top: 15px;
           border-radius: 10px;
           outline: none;
+          font-size: 20px;
         }
       }
       &-register {
